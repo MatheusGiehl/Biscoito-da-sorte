e34a9443fef09319a7462cc797c30feae96bacf7
Matheus <giehlmatheus96@gmail.com>
2022-11-21 22:09:50 -0300

    update  App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state= {};
+
+    this.frases = [
+      'Siga os bons e aprenda com eles.',
+      'O bom-senso vale mais do que muito conhecimento.',
+      'O riso é a menor distância entre duas pessoas.',
+      'Deixe de lado as preocupações e seja feliz.',
+      'Realize o óbvio, pense no improvável e conquiste o impossível.',
+      'Acredite em milagres, mas não dependa deles.',
+      'A maior barreira para o sucesso é o medo do fracasso.'
+    ];
+  }
+
+  quebraBiscoito() {
+    console.log("quebrou");
+  }
+
+  render() {
+    return (
+    <div className="container">
+      <img 
+         className="imagem"
+         src={require('./assets/biscoito.png')}
+         alt='biscoito'
+      />
+      <Botao nome="Abrir Biscoito" acaoBtn={this.quebraBiscoito}/>
+      <h3 className="textoFrase">Frase aleatória... </h3>
     </div>
-  );
+    );
+  }
+}
+
+class Botao extends Component {
+  render () {
+    return (
+      <div>
+        <button
+         onCLick={this.props.acaoBtn} 
+         className="botao btn"
+         >
+          {this.props.nome}
+          </button>
+      </div>
+    )
+  }
 }
 
 export default App;
